dcf0699155873fb0f94833c4123b6ad992362997
kurtis <kurt.nistelberger@hotmail.com>
2020-02-25 10:44:18 +0100

    update secure pin storage

--- app/src/main/java/com/example/firsttestapp/MainActivity.java
+++ app/src/main/java/com/example/firsttestapp/MainActivity.java
@@ -1,72 +1,61 @@
 package com.example.firsttestapp;
 
-import android.app.Application;
-import android.content.Context;
 import android.os.Bundle;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
-import java.security.UnrecoverableEntryException;
-import java.security.cert.CertificateException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    public static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
-    }
+    private PinHandling pin_handler;
+    private Button btnFirstPin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // create new pinhandler instance
+        pin_handler = new PinHandling(MainActivity.this.getFilesDir());
+        if( !pin_handler.checkIfPinExists() ){
+            // first login, we have to generate a login PIN
+            setContentView(R.layout.pingeneration);
+
+            btnFirstPin =  findViewById(R.id.btnFirstPinGeneration);
+            EditText txtPin =  findViewById(R.id.txtPIN);
+            if (txtPin == null){
+                // not found, should never happen
+            }
+
+            txtPin.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    if ( editable.length() == 4) {
+                        btnFirstPin.setEnabled(true);
+                    }
+                    else {
+                        btnFirstPin.setEnabled(false);
+                    }
+                }
+            });
+        }
+        else {
+            // pin already created
+            setContentView(R.layout.activity_main);
+        }
     }
 
     @Override
@@ -91,109 +80,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //**********************************************************************************************
-    // KeyStore Code
-    //**********************************************************************************************
-
-    private static final String PIN_STORAGE_ALIAS = "PinStorage";
-    private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
-    private KeyStore keyStore;
-
-    private byte[] encryption;
-    private byte[] iv;
-
-    // ENCRYPTION CODE
-
-    private SecretKey getSecretKeyEnc(final String alias) throws NoSuchProviderException,
-            InvalidAlgorithmParameterException, NoSuchAlgorithmException
-    {
-        final KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE);
-
-        keyGenerator.init(new KeyGenParameterSpec.Builder(alias,
-                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .build());
-
-        return keyGenerator.generateKey();
-    }
-
-    byte[] encryptText(final String alias, final String textToEncrypt)
-            throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException,
-            InvalidKeyException, IOException, InvalidAlgorithmParameterException,
-            BadPaddingException, IllegalBlockSizeException {
-
-        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, getSecretKeyEnc(alias));
-
-        iv = cipher.getIV();
-
-        return (encryption = cipher.doFinal(textToEncrypt.getBytes("UTF-8")));
-    }
-
-    // DECRYPTION CODE
-
-    private void initKeyStore() throws KeyStoreException, CertificateException,
-            NoSuchAlgorithmException, IOException {
-        keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
-        keyStore.load(null);
-    }
-
-    private SecretKey getSecretKeyDec(final String alias) throws NoSuchAlgorithmException,
-            UnrecoverableEntryException, KeyStoreException {
-        return ((KeyStore.SecretKeyEntry) keyStore.getEntry(alias, null)).getSecretKey();
-    }
-
-    String decryptData(final String alias, final byte[] encryptedData, final byte[] encryptionIv)
-            throws UnrecoverableEntryException, NoSuchAlgorithmException, KeyStoreException,
-            NoSuchPaddingException, InvalidKeyException, IOException,
-            BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-
-        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        final GCMParameterSpec spec = new GCMParameterSpec(128, encryptionIv);
-        cipher.init(Cipher.DECRYPT_MODE, getSecretKeyDec(alias), spec);
-
-        return new String(cipher.doFinal(encryptedData), "UTF-8");
-    }
-
-
-    //**********************************************************************************************
-    // Read/Write Code
-    //**********************************************************************************************
-
-    private void writeToFile(byte[] data) {
-        try{
-            File path = MainActivity.this.getFilesDir();
-            System.out.println(path.toString());
-            File file = new File(path, "store.enc");
-            FileOutputStream stream = new FileOutputStream(file);
-            stream.write(data);
-            stream.close();
-        }
-        catch (IOException e) {
-            System.out.println("Could not write to File");
-        }
-    }
-
-    private byte[] readFromFile() {
-        try{
-            File path = MainActivity.this.getFilesDir();
-            System.out.println(path.toString());
-            File file = new File(path, "store.enc");
-
-            byte[] bytes = new byte[(int)file.length()];
-
-            FileInputStream in = new FileInputStream(file);
-            in.read(bytes);
-            in.close();
-            return bytes;
-        }
-        catch (IOException e) {
-            System.out.println("Could not write to File");
-        }
-        return null;
-    }
 
     //**********************************************************************************************
     // Application Code
@@ -299,10 +185,12 @@ public class MainActivity extends AppCompatActivity {
         TextView txtSuccess = findViewById(R.id.txtSuccess);
         if(pin_round == 4){
             // do checking of PIN
-
-
-            // if false pin
-            txtSuccess.setText("Wrong Password: " + inputpin);
+            if(pin_handler.verifyPIN(inputpin)){
+                txtSuccess.setText("Correct Password: " + inputpin);
+            }
+            else {
+                txtSuccess.setText("Wrong Password: " + inputpin);
+            }
             resetAllRbtn();
             pin_round = 0;
             inputpin = "";
@@ -312,70 +200,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void encryptMessage(View view) {
+    public void firstLoginDone(View view) {
 
-        try {
-            TextView txtContent = findViewById(R.id.txtContent);
-            encryptText(PIN_STORAGE_ALIAS, txtContent.getText().toString());
+        EditText txtPin = findViewById(R.id.txtPIN);
+        if (pin_handler.storeNewPin(txtPin.getText().toString())) {
+            // switch to MainView
+            setContentView(R.layout.activity_main);
+        }
 
-            TextView txtviewIV = findViewById(R.id.txtIV);
-            txtContent.setText(bytesToHex(encryption));
-            txtviewIV.setText(bytesToHex(iv));
-        }
-        catch(Exception ex){
-            System.out.println("Could not encrypt data");
-        }
     }
 
-    public void decryptMessage(View view) {
-
-        try {
-            TextView txtContent = findViewById(R.id.txtContent);
-            TextView txtIV = findViewById(R.id.txtIV);
-
-            String enc_data = txtContent.getText().toString();
-            String enc_iv = txtIV.getText().toString();
-
-            initKeyStore();
-            String msg = decryptData(PIN_STORAGE_ALIAS, hexStringToByteArray(enc_data), hexStringToByteArray(enc_iv));
-
-            txtContent.setText(msg);
-            txtIV.setText("");
-        }
-        catch(Exception ex) {
-            System.out.println("Could not decrypt data");
-        }
-    }
-
-    public void saveMessage(View view) {
-
-        if(iv == null)
-            return;
-
-        System.out.println("IV length: " + iv.length);
-
-        byte[] data = new byte[iv.length + encryption.length];
-        System.arraycopy(iv, 0, data, 0, iv.length);
-        System.arraycopy(encryption, 0, data, iv.length, encryption.length);
-        writeToFile(data);
-    }
-
-    public void loadMessage(View view) {
-
-        byte[] data = readFromFile();
-        if(data == null)
-            return;
-
-        iv = new byte[12];
-        encryption = new byte[data.length-12];
-
-        System.arraycopy(data, 0, iv, 0, 12);
-        System.arraycopy(data, 12, encryption, 0, data.length-12);
-
-        TextView txtviewContent = findViewById(R.id.txtContent);
-        TextView txtviewIV = findViewById(R.id.txtIV);
-        txtviewContent.setText(bytesToHex(encryption));
-        txtviewIV.setText(bytesToHex(iv));
-
-    }
 }
---
+++ app/src/main/java/com/example/firsttestapp/PinHandling.java
@@ -0,0 +1,226 @@
+package com.example.firsttestapp;
+
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+
+public class PinHandling {
+
+    private static final String PIN_STORAGE_ALIAS = "PinStorage";
+    private static final String TRANSFORMATION = "AES/GCM/NoPadding";
+    private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
+    private KeyStore keyStore;
+    private byte[] encryption;
+    private byte[] iv;
+
+    File path = null;
+    PinHandling(File path1){
+        path = path1;
+    }
+
+    //**********************************************************************************************
+    // Encryption Code
+    //**********************************************************************************************
+    private boolean encryptText(final String alias, final String textToEncrypt, boolean randomIV)
+            throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException,
+            InvalidKeyException, IOException, InvalidAlgorithmParameterException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        // needs to be disabled in order to allow custom IVs, this is not recommended but
+        // necessary to get two times the same encrypted text..
+        // KeyGenParameterSpec.Builder.setRandomizedEncryptionRequired
+
+        SecretKey skey = null;
+        GCMParameterSpec gcmspec;
+        if(randomIV) {
+            // create random IV
+            iv = new byte[12];
+            SecureRandom r = new SecureRandom();
+            r.nextBytes(iv);
+            gcmspec = new GCMParameterSpec(96, iv);
+
+            // create new key
+            skey = getSecretKeyEnc(alias);
+        }
+        else{
+            // reuse iv and key
+            gcmspec = new GCMParameterSpec(96, iv);
+            try {
+                skey = getSecretKeyDec(alias);
+            }
+            catch (Exception ex ){
+                System.out.println("Could not reuse key");
+                return false;
+            }
+        }
+
+        // encryption code
+        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, skey, gcmspec);
+        iv = cipher.getIV();
+        encryption = cipher.doFinal(textToEncrypt.getBytes("UTF-8"));
+
+        return true;
+    }
+
+
+    private SecretKey getSecretKeyEnc(final String alias) throws NoSuchProviderException,
+            InvalidAlgorithmParameterException, NoSuchAlgorithmException
+    {
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE);
+
+        keyGenerator.init(new KeyGenParameterSpec.Builder(alias,
+                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .setRandomizedEncryptionRequired(false)
+                .build());
+
+        return keyGenerator.generateKey();
+    }
+
+    //**********************************************************************************************
+    // Decryption Code
+    //**********************************************************************************************
+    private void initKeyStore() throws KeyStoreException, CertificateException,
+            NoSuchAlgorithmException, IOException {
+        keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
+        keyStore.load(null);
+    }
+
+    private SecretKey getSecretKeyDec(final String alias) throws NoSuchAlgorithmException,
+            UnrecoverableEntryException, KeyStoreException, CertificateException, IOException {
+        initKeyStore();
+        return ((KeyStore.SecretKeyEntry) keyStore.getEntry(alias, null)).getSecretKey();
+    }
+
+    /*
+    private String decryptData(final String alias, final byte[] encryptedData, final byte[] encryptionIv)
+            throws UnrecoverableEntryException, NoSuchAlgorithmException, KeyStoreException,
+            NoSuchPaddingException, InvalidKeyException, IOException,
+            BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+
+        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        final GCMParameterSpec spec = new GCMParameterSpec(128, encryptionIv);
+        //cipher.init(Cipher.DECRYPT_MODE, getSecretKeyDec(alias), spec);
+
+        return new String(cipher.doFinal(encryptedData), "UTF-8");
+    }*/
+
+
+    //**********************************************************************************************
+    // Read/Write Code
+    //**********************************************************************************************
+    private void writeToFile(byte[] data) {
+        try{
+            File file = new File(path, "store.enc");
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.write(data);
+            stream.close();
+        }
+        catch (IOException e) {
+            System.out.println("Could not write to File");
+        }
+    }
+
+    private byte[] readFromFile() {
+        try{
+            File file = new File(path, "store.enc");
+            byte[] bytes = new byte[(int)file.length()];
+            FileInputStream in = new FileInputStream(file);
+            in.read(bytes);
+            in.close();
+            return bytes;
+        }
+        catch (IOException e) {
+            System.out.println("Could not write to File");
+        }
+        return null;
+    }
+
+    //**********************************************************************************************
+    // Application Code
+    //**********************************************************************************************
+    public boolean checkIfPinExists(){
+
+        // check if file exists, if yes, we already have a PIN defined
+        File file = new File(path, "store.enc");
+        if (file.exists())
+            return true;
+        return false;
+    }
+
+    public boolean storeNewPin(String pin){
+
+        // encrypt and store PIN
+        try {
+            if( !encryptText(PIN_STORAGE_ALIAS, pin, true))
+                return false;
+        }
+        catch(Exception ex) {
+            System.out.println("Could not encrypt data");
+            return false;
+        }
+
+        byte[] data = new byte[iv.length + encryption.length];
+        System.arraycopy(iv, 0, data, 0, iv.length);
+        System.arraycopy(encryption, 0, data, iv.length, encryption.length);
+        writeToFile(data);
+        return true;
+    }
+
+    public boolean verifyPIN(String input){
+
+        // load file content
+        byte[] data = readFromFile();
+        if(data == null)
+            return false;
+
+        // write to public iv and private enc
+        iv = new byte[12];
+        byte[] enc_vf = new byte[data.length-12];
+        System.arraycopy(data, 0, iv, 0, 12);
+        System.arraycopy(data, 12, enc_vf, 0, data.length-12);
+
+        // encrypt PIN
+        try {
+            encryptText(PIN_STORAGE_ALIAS, input, false);
+
+            if(Arrays.equals(enc_vf, encryption))
+                return true;
+        }
+        catch(Exception ex){
+            System.out.println("Could not encrypt data");
+            return false;
+        }
+
+        return false;
+    }
+
+}
